0d262bb4103b3e1bc5725174f4f93b373bdf2a32
feef-source <sharifibrahim@jayps.org>
2025-03-05 14:13:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,35 @@ def run_server():
     httpd.serve_forever()
 
 run_server()
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        url_parts = urllib.parse.urlparse(self.path)
+        if url_parts.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open('index.html', 'rb') as file:
+                self.wfile.write(file.read())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
+def run_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print('Server running on port 8000...')
+    httpd.serve_forever()
+
+run_server()
+import ssl
+
+    # Fix for sites that refuse to connect
+    context = ssl._create_unverified_context()
+    httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='certificate.crt', keyfile='host.key', ssl_version=ssl.PROTOCOL_TLSv1_2, context=context)
+    print('SSL connection failed. Trying without SSL...')
+    httpd.socket = httpd.socket
